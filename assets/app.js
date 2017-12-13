1f6abb178a607eded3b80eefc7eeb71a7bf5e95f
lanchondo <30990366+lanchondo@users.noreply.github.com>
2017-12-13 16:55:41 -0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBAOJ9b7g4q-j9iXfauDmEA8GucGkqjvcc",
-  authDomain: "fir-trainapp.firebaseapp.com",
-  databaseURL: "https://fir-trainapp.firebaseio.com",
-  storageBucket: "fir-trainapp.appspot.com",
-  messagingSenderId: "183389769681"
+  apiKey: "AIzaSyCgTI0irXOmlGsEONEcdZ40mMhz1335Iwc",
+  authDomain: "train-6e23c.firebaseapp.com",
+  databaseURL: "https://train-6e23c.firebaseio.com",
+  projectId: "train-6e23c",
+  storageBucket: "train-6e23c.appspot.com",
+  messagingSenderId: "87410090111"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
